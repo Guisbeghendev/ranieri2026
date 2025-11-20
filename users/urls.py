abb8cc0d4279828bca7fc3b9afd78f1e5d674c4a
Guisbeghendev <guisbeghendev@gmail.com>
2025-11-20 16:30:08 -0300

    esqueci minha senha ok

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -12,28 +12,31 @@ urlpatterns = [
     # ROTAS DE CADASTRO
     path('register/', views.registration_create, name='register'),
 
-    # ROTAS DE REDEFINIÇÃO DE SENHA (Mantidas)
+    # ROTAS DE REDEFINIÇÃO DE SENHA (CORRIGIDA)
     path(
         'password_reset/',
         auth_views.PasswordResetView.as_view(
-            template_name='users/password_reset_form.html',
-            email_template_name='users/password_reset_email.html'
+            # CORREÇÃO CRÍTICA: Deve-se usar reverse_lazy para evitar que 'users' seja interpretado como um protocolo,
+            # o que causou o DisallowedRedirect.
+            success_url=reverse_lazy('users:password_reset_done'),
+            template_name='registration/password_reset_form.html',
+            email_template_name='registration/password_reset_email.html'
         ),
         name='password_reset'
     ),
     path(
         'password_reset/done/',
-        auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+        auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
         name='password_reset_done'
     ),
     path(
         'reset/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+        auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
         name='password_reset_confirm'
     ),
     path(
         'reset/done/',
-        auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
         name='password_reset_complete'
     ),
 

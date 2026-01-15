e54db1f3ed64564e95a0fd950c4a60a8f97d4b6e
Guisbeghendev <guisbeghendev@gmail.com>
2026-01-15 16:35:08 -0300

    app ajustes geerais IX

--- repositorio/routing.py
+++ repositorio/routing.py
@@ -2,7 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    # Rota atualizada para aceitar o slug da galeria, permitindo conexão com o grupo correto.
-    # Opcional para suportar tanto a visualização da galeria quanto o upload geral.
-    re_path(r'ws/repositorio/galerias/(?P<slug>[\w-]+)?/?$', consumers.GaleriaConsumer.as_asgi()),
+    # A URL deve conter 'repositorio-admin' para coincidir com a chamada do front-end
+    re_path(r'ws/repositorio-admin/galerias/(?P<slug>[\w-]+)/?$', consumers.GaleriaConsumer.as_asgi()),
 ]
